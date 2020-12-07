982bc8d746da2486793d4638ad7fa50259651506
SteffiZwing <56028843+SteffiZwing@users.noreply.github.com>
2020-12-07 12:20:46 +0100

    Update Sceneloader.cs

--- Assets/MyGame/Scripts/Sceneloader.cs
+++ Assets/MyGame/Scripts/Sceneloader.cs
@@ -7,17 +7,20 @@ using System;
 // AM  Keyword Klassenname  Elternkasse
 public class Sceneloader : MonoBehaviour
 {
-    //Declare Global values and object references
+    //the Global values and object references gets declared
+    // Am  Datentyp variablenName wird rechten Wert zugewiesen
     public int numberPress = 0;
     public InputField nameInput;
     public PlayerName playerName;
     public GameObject buttonPressText;
 
     //gets called in the onclick function of the Button
+    // Am returntype Methodenaufruf mit leerer Parameterliste
     public void addButtenPress()
     {
         numberPress++; //add 1 to the counting value
         buttonPressText.GetComponent<Text>().text = numberPress.ToString(); //Display the ammount in textfield
+        // if statement Variable Vergleichsoperator und Wert 5
         if (numberPress == 5) //check if button is pressed 5 times
         {
             LoadNextScene("SceneAdd");
@@ -25,11 +28,12 @@ public class Sceneloader : MonoBehaviour
     }
 
     //switches scene based on scene names
+    // Am  return type Methodenaufruf  
     public void LoadNextScene(string name)
     {
         SceneManager.LoadScene(name);
     }
-    public void UpdatePlayerName() //safe playername to scriptable game object
+    public void UpdatePlayerName() //safes the playername to scriptable game object
     {
         playerName.runtimevalue = nameInput.text;
     }
