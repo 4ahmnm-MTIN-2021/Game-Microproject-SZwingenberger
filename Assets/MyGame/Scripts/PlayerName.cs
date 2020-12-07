970c6a6a407bca9c5c80d93196e33160a71b3f6d
SteffiZwing <56028843+SteffiZwing@users.noreply.github.com>
2020-12-07 12:12:51 +0100

    Update PlayerName.cs

--- Assets/MyGame/Scripts/PlayerName.cs
+++ Assets/MyGame/Scripts/PlayerName.cs
@@ -7,13 +7,14 @@ using System;
 public class PlayerName : ScriptableObject, ISerializationCallbackReceiver
 {
     // AM  variablentyp variablenname
-    public string runtimevalue; //value changes while game is played
+    public string runtimevalue; //value changes while the game is played
     public string initialvalue;
     public void OnBeforeSerialize()
     {
 
     }
-    public void OnAfterDeserialize() //reset value when the game ends
+    // Am  return type Methode       
+    public void OnAfterDeserialize() //resets value when the game ends
     {
         runtimevalue = initialvalue;
     }
